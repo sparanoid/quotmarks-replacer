487da22c24eb2016329592ff4cab0af040b68336
Sparanoid <Sparanoid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-14 13:27:33 +0000

    git-svn-id: http://plugins.svn.wordpress.org/quotmarks-replacer/trunk@125773 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qmr.php
+++ qmr.php
@@ -8,52 +8,6 @@ Author: Sparanoid
 Author URI: http://sparanoid.com/
 */
 
-/*
-Following is the old way to replace the wptexturized contents:
-
-function qmr_replace($the_content) {
-    $the_content = str_replace("&#8220;", "&quot;", $the_content);
-    $the_content = str_replace("&#8221;", "&quot;", $the_content);
-    $the_content = str_replace("&#8243;", "&quot;", $the_content);
-    $the_content = str_replace("&#8216;", "'", $the_content);
-    $the_content = str_replace("&#8217;", "'", $the_content);
-    $the_content = str_replace("&#8242;", "'", $the_content);
-    $the_content = str_replace("&#8230;", "...", $the_content);
-    $the_content = str_replace("&#8211;", "--", $the_content);
-    $the_content = str_replace("&#8212;", "---", $the_content);
-	return $the_content;
-}
-
-function qmr_filter($content) {
-	return qmr_replace($content);
-}
-
-add_filter ('bloginfo','qmr_filter');
-add_filter ('the_title','qmr_filter');
-add_filter ('the_content','qmr_filter');
-add_filter ('the_excerpt','qmr_filter');
-add_filter ('category_description','qmr_filter');
-add_filter ('comment_author','qmr_filter');
-add_filter ('comment_excerpt','qmr_filter');
-add_filter ('comment_text','qmr_filter');
-add_filter ('list_cats','qmr_filter');
-add_filter ('single_post_title','qmr_filter');
-
-
-// This is a new way to remove the wptexturize filter:
-remove_filter ('bloginfo','wptexturize');
-remove_filter ('the_title','wptexturize');
-remove_filter ('the_content','wptexturize');
-remove_filter ('the_excerpt','wptexturize');
-remove_filter ('category_description','wptexturize');
-remove_filter ('comment_author','wptexturize');
-remove_filter ('comment_excerpt','wptexturize');
-remove_filter ('comment_text','wptexturize');
-remove_filter ('list_cats','wptexturize');
-remove_filter ('single_post_title','wptexturize');
-*/
-
-// This is a new way to remove the wptexturize filter:
 $qmr_work_tags = array(
 	'bloginfo',
 	'comment_author',
