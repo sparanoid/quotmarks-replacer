bf0353ac49031230d182e834888e0b6c79c366bd
Sparanoid <Sparanoid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-16 06:57:44 +0000

    Bump version to 2.6.1
    git-svn-id: http://plugins.svn.wordpress.org/quotmarks-replacer/trunk@438917 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qmr.php
+++ qmr.php
@@ -3,32 +3,32 @@
 Plugin Name: Quotmarks Replacer
 Plugin URI: http://sparanoid.com/work/quotmarks-replacer/
 Description: A plugin disables wptexturize founction that keeps all quotation marks and suspension points in half-width form.
-Version: 2.6
+Version: 2.6.1
 Author: Sparanoid
 Author URI: http://sparanoid.com/
 */
 
 $qmr_work_tags = array(
-	'the_title',				// http://codex.wordpress.org/Function_Reference/the_title
-	'the_content',				// http://codex.wordpress.org/Function_Reference/the_content
-	'the_excerpt',				// http://codex.wordpress.org/Function_Reference/the_excerpt
-	// 'list_cats',				Deprecated. http://codex.wordpress.org/Function_Reference/list_cats
-	'single_post_title',		// http://codex.wordpress.org/Function_Reference/single_post_title
-	'comment_author',			// http://codex.wordpress.org/Function_Reference/comment_author
-	'comment_text',				// http://codex.wordpress.org/Function_Reference/comment_text
-	// 'link_name',				Deprecated.
-	// 'link_notes',			Deprecated.
-	'link_description',			// Deprecated, but still widely used.
-	'bloginfo',					// http://codex.wordpress.org/Function_Reference/bloginfo
-	'wp_title',					// http://codex.wordpress.org/Function_Reference/wp_title
-	'term_description',			// http://codex.wordpress.org/Function_Reference/term_description
-	'category_description',		// http://codex.wordpress.org/Function_Reference/category_description
-	'widget_title',				// Used by all widgets in themes
-	'widget_text'				// Used by all widgets in themes
-	);
+  'the_title',        // http://codex.wordpress.org/Function_Reference/the_title
+  'the_content',        // http://codex.wordpress.org/Function_Reference/the_content
+  'the_excerpt',        // http://codex.wordpress.org/Function_Reference/the_excerpt
+  // 'list_cats',       Deprecated. http://codex.wordpress.org/Function_Reference/list_cats
+  'single_post_title',    // http://codex.wordpress.org/Function_Reference/single_post_title
+  'comment_author',     // http://codex.wordpress.org/Function_Reference/comment_author
+  'comment_text',       // http://codex.wordpress.org/Function_Reference/comment_text
+  // 'link_name',       Deprecated.
+  // 'link_notes',      Deprecated.
+  'link_description',     // Deprecated, but still widely used.
+  'bloginfo',         // http://codex.wordpress.org/Function_Reference/bloginfo
+  'wp_title',         // http://codex.wordpress.org/Function_Reference/wp_title
+  'term_description',     // http://codex.wordpress.org/Function_Reference/term_description
+  'category_description',   // http://codex.wordpress.org/Function_Reference/category_description
+  'widget_title',       // Used by all widgets in themes
+  'widget_text'       // Used by all widgets in themes
+  );
 
 foreach ( $qmr_work_tags as $qmr_work_tag ) {
-	remove_filter ($qmr_work_tag, 'wptexturize');
+  remove_filter ($qmr_work_tag, 'wptexturize');
 }
 
 ?>
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: Sparanoid
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=VJRCEYRS8GTYE&lc=C2&item_name=Plugin%20Donation&button_subtype=services&currency_code=USD&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted
 Tags: admin, administration, comment, comments, content, contents, excerpt, feed, feeds, html, page, pages, plugin, plugins, post, posts, template, text, title, writing, wptexturize, wpmu
 Requires at least: 1.5
-Tested up to: 3.2
-Stable tag: 2.6
+Tested up to: 3.3-aortic-dissection
+Stable tag: 2.6.1
 
 Quotmarks Replacer disables wptexturize function that keeps all quotation marks and suspension points in half-width form.
 
@@ -33,6 +33,10 @@ Old-styled WordPress Mu:
 
 
 == Changelog ==  
+
+= 2.6.1 =
+* Compatibility check for 3.3-aortic-dissection, nothing new, just bump version to tell everyone this plugin still works.
+* Updated setup instructions
 
 = 2.6 =
 * Compatibility check for 3.2, nothing new, just bump version to tell everyone this plugin still works.
