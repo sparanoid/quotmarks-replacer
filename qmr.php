7383963594a79c6e4bbb764e956bcdb50f09a1f7
Sparanoid <Sparanoid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-16 16:49:35 +0000

    Updated assets
    git-svn-id: http://plugins.svn.wordpress.org/quotmarks-replacer/trunk@599718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qmr.php
+++ qmr.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Quotmarks Replacer
 Plugin URI: http://sparanoid.com/work/quotmarks-replacer/
-Description: A plugin disables wptexturize founction that keeps all quotation marks and suspension points in half-width form.
+Description: A plugin disables wptexturize function that keeps all quotation marks and suspension points in half-width form.
 Version: 2.6.5
 Author: Tunghsiao Liu
 Author URI: http://sparanoid.com/
@@ -12,7 +12,7 @@ License: GPLv2 or later
   Copyright 2012 Tunghsiao Liu, aka. Sparanoid (info@sparanoid.com)
 
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as 
+  it under the terms of the GNU General Public License, version 2, as
   published by the Free Software Foundation.
 
   This program is distributed in the hope that it will be useful,
